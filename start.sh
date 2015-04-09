573ae0f013192a72df65fa78bb3dc679091e17bc
Forrest Chromebook <forrestp@nvbots.com>
2015-04-09 07:53:53 -0400

    add username and password

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'resin-mjpgstreamer launching...'
-/app/mjpg-streamer/mjpg_streamer -i "/app/mjpg-streamer/input_uvc.so -f 10 -r 1280x720" -o "/app/mjpg-streamer/output_http.so -w /app/mjpg-streamer/www"
+/app/mjpg-streamer/mjpg_streamer -i "/app/mjpg-streamer/input_uvc.so -f 10 -r 1280x720" -o "/app/mjpg-streamer/output_http.so -w /app/mjpg-streamer/www -c $USERNAME:$PASSWORD"
 echo 'resin-mjpgstreamer sleeping for terminal access'
 sleep 600
 echo 'resin-mjpgstreamer exiting'
